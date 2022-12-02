47722e7f7ecfdb402918da9095f716a26ec49606
alphons <alphons@heijden.com>
2022-12-02 18:13:14 +0100

    init

---
+++ src/BsonDocumentJsonConverter.cs
@@ -0,0 +1,208 @@
+﻿
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Globalization;
+
+using MongoDB.Bson;
+
+namespace MongoDB.MvcCore;
+
+public class Serializer
+{
+	/// <summary>
+	/// Give color chars on console output
+	/// </summary>
+	public static bool Colorize { get; set; }
+
+	private const string SPACES = "                                                                                ";
+
+	public static string ToJson(List<BsonDocument> bsonDocuments, JsonSerializerOptions? options)
+	{
+		var WriteIndented = options != null && options.WriteIndented;
+		var sb = new StringBuilder();
+		sb.Append('[');
+		if (WriteIndented)
+			sb.AppendLine();
+		for (int intI = 0; intI < bsonDocuments.Count; intI++)
+		{
+			sb.Append(Serializer.ToJson(1, bsonDocuments[intI], options));
+			if (intI < (bsonDocuments.Count - 1))
+				sb.Append(',');
+			if (WriteIndented)
+				sb.AppendLine();
+		}
+		sb.Append(']');
+		if (WriteIndented)
+			sb.AppendLine();
+		return sb.ToString();
+	}
+
+	private static string Escape(object? o)
+	{
+		return $"{o}"
+			.Replace("\\", "\\\\")
+			.Replace("\"", "\\\"")
+			.Replace("\t", "\\t")
+			.Replace("\b", "\\b")
+			.Replace("\f", "\\f")
+			.Replace("\n", "\\n")
+			.Replace("\r", "\\r")
+			;
+	}
+
+	private static string Indent(int Depth)
+	{
+		return SPACES[0..(2 * Depth)];  // TABS[0..Depth]
+	}
+
+	// Names and values MUST be encoded using doublequote chars
+
+	public static string ToJson(int Depth, BsonValue bsonValue, JsonSerializerOptions? options)
+	{
+		var WriteIndented = options != null && options.WriteIndented;
+		var I = WriteIndented ? Indent(Depth) : string.Empty;	// Indent
+		var II = WriteIndented ? Indent(Depth+1) : string.Empty; // Indent + 1
+		var N = WriteIndented ? Environment.NewLine : string.Empty; // NewLine
+		var S = WriteIndented ? " " : string.Empty; // Space
+		var sb = new StringBuilder();
+
+		if (bsonValue.IsBsonDocument)
+		{
+			sb.Append($"{I}{{{N}");
+			var bsonDocument = bsonValue.AsBsonDocument;
+			for (int intI = 0; intI < bsonDocument.ElementCount; intI++)
+			{
+				BsonElement element = bsonDocument.Elements.ElementAt(intI);
+
+				if(Serializer.Colorize)
+					sb.Append($"{II}\u0084\"{element.Name}\"\u0080:{S}");
+				else
+					sb.Append($"{II}\"{element.Name}\":{S}");
+				if(WriteIndented && (element.Value.IsBsonDocument || element.Value.IsBsonArray))
+					sb.AppendLine();
+
+				sb.Append($"{ToJson(Depth + 1, element.Value, options)}");
+
+				if (intI < (bsonDocument.ElementCount - 1))
+					sb.Append(',');
+				sb.Append(N);
+			}
+			sb.Append($"{I}}}");
+
+			return sb.ToString();
+		}
+
+		if (bsonValue.IsBsonArray)
+		{
+			sb.AppendLine($"{I}[");
+			for (int intJ = 0; intJ < bsonValue.AsBsonArray.Count; intJ++)
+			{
+				var val = bsonValue.AsBsonArray[intJ];
+				if (WriteIndented && !val.IsBsonDocument && !val.IsBsonArray)
+					sb.Append(II);
+				sb.Append($"{ToJson(Depth + 1, val, options)}");
+				if (intJ < (bsonValue.AsBsonArray.Count - 1))
+					sb.Append(',');
+				sb.Append(N);
+			}
+			sb.Append($"{I}]");
+			return sb.ToString();
+		}
+
+		var dotNetValue = BsonTypeMapper.MapToDotNetValue(bsonValue);
+
+		if (Serializer.Colorize)
+		{
+			if (dotNetValue is BsonRegularExpression)
+				dotNetValue = dotNetValue.ToString();
+
+			if (dotNetValue is string)
+				dotNetValue = $"\u0081\"{Escape(dotNetValue)}\"\u0080";
+
+			if (dotNetValue is Guid)
+				dotNetValue = $"\u008aUUID(\"{ dotNetValue}\")\u0080";
+
+			if (dotNetValue is ObjectId)
+				dotNetValue = $"\u008aObjectId(\"{dotNetValue}\")\u0080";
+
+			if (dotNetValue is byte[] bindata)
+				dotNetValue = "\u008aBinary(Buffer.from(\"" + BitConverter.ToString(bindata).ToLower().Replace("-", string.Empty) + "\", \"hex\"), 0)\u0080";
+
+			if (bsonValue is BsonTimestamp timestamp)
+				dotNetValue = $"\u008aTimestamp({{ t: {timestamp.Timestamp}, i: {timestamp.Increment}}})\u0080";
+
+			if (dotNetValue is bool boolValue)
+				dotNetValue = $"\u0083{boolValue.ToString().ToLower()}\u0080";
+
+			if (dotNetValue == null || dotNetValue is BsonNull)
+				dotNetValue = $"null";
+
+			if (dotNetValue is DateTime dtm)
+				dotNetValue = FormattableString.Invariant($"\u008aISODate(\"{dtm:yyyy-MM-ddTHH:mm:ss.fffZ}\")\u0080");// dtm.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+
+
+			if (dotNetValue is int || dotNetValue is decimal || dotNetValue is double || dotNetValue is Decimal128)
+				dotNetValue = FormattableString.Invariant($"\u0082{dotNetValue}\u0080");
+		}
+		else
+		{
+			if (dotNetValue is ObjectId)
+				dotNetValue = dotNetValue.ToString();
+
+			if (dotNetValue is DateTime dtm)
+				dotNetValue = dtm.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+
+			if (dotNetValue is BsonRegularExpression)
+				dotNetValue = dotNetValue.ToString();
+
+			if (dotNetValue is byte[] bindata)
+				dotNetValue = BitConverter.ToString(bindata).ToLower().Replace("-", string.Empty);
+
+			if (dotNetValue is BsonTimestamp)
+				dotNetValue = dotNetValue.ToString();
+
+			if (dotNetValue is Guid)
+				dotNetValue = dotNetValue.ToString();
+
+			if (dotNetValue is string)
+				dotNetValue = $"\"{Escape(dotNetValue)}\"";
+
+			if (dotNetValue is bool boolValue)
+				dotNetValue = boolValue.ToString().ToLower();
+
+			if (dotNetValue == null || dotNetValue is BsonNull)
+				dotNetValue = $"null";
+		}
+
+		sb.Append(CultureInfo.InvariantCulture, $"{dotNetValue}");
+
+		return sb.ToString();
+	}
+}
+
+public class BsonDocumentJsonConverter : JsonConverter<BsonDocument>
+{
+	public override BsonDocument? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Write(Utf8JsonWriter writer, BsonDocument bsonDocument, JsonSerializerOptions options)
+	{
+		writer.WriteRawValue(Serializer.ToJson(0, bsonDocument, options));
+	}
+}
+
+public class BsonDocumentsJsonConverter : JsonConverter<List<BsonDocument>>
+{
+	public override List<BsonDocument>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Write(Utf8JsonWriter writer, List<BsonDocument> bsonDocuments, JsonSerializerOptions? options)
+	{
+		writer.WriteRawValue(Serializer.ToJson(bsonDocuments, options));
+	}
+}
---
+++ src/BsonJsonExtensions.cs
@@ -0,0 +1,251 @@
+﻿using System.Text.Json;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDB.MvcCore;
+public static class BsonJsonExtensions
+{
+	/// <summary>
+	/// Add BsonDocumentJsonConverter and BsonDocumentsJsonConverter to IMvcCoreBuilder
+	/// </summary>
+	/// <param name="builder"></param>
+	/// <returns></returns>
+	public static IMvcCoreBuilder AddBsonJsonConverters(this IMvcCoreBuilder builder)
+	{
+		return builder.AddJsonOptions(options =>
+		{
+			options.JsonSerializerOptions.Converters.Add(new BsonDocumentJsonConverter());
+			options.JsonSerializerOptions.Converters.Add(new BsonDocumentsJsonConverter());
+
+			options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+			options.JsonSerializerOptions.PropertyNamingPolicy = null;
+		});
+	}
+
+	public async static Task<BsonDocument> RunCommandAsync(this IMongoDatabase db, string JsonDocument, ReadPreference? readPreference = null, CancellationToken cancellationToken = default)
+	{
+		return await db.RunCommandAsync<BsonDocument>(BsonDocument.Parse(JsonDocument), readPreference, cancellationToken);
+	}
+
+	public async static Task<BsonValue> InsertOneAsync(this IMongoCollection<BsonDocument> collection, string JsonDocument, InsertOneOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		var doc = BsonDocument.Parse(JsonDocument);
+		await collection.InsertOneAsync(doc, options, cancellationToken);
+		return doc["_id"];
+	}
+
+	public async static Task<BsonValue> InsertOneAsync(this IMongoCollection<BsonDocument> collection, object objA, InsertOneOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		return await InsertOneAsync(collection, System.Text.Json.JsonSerializer.Serialize(objA), options, cancellationToken);
+	}
+
+	public async static Task<List<BsonValue>> InsertManyAsync(this IMongoCollection<BsonDocument> collection, string JsonDocuments, InsertManyOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+		await collection.InsertManyAsync(bsonDocuments, options, cancellationToken);
+		return bsonDocuments.Select(x => x["_id"]).ToList();
+	}
+
+	public async static Task<DeleteResult> DeleteOneAsync(this IMongoCollection<BsonDocument> collection, object objA, CancellationToken cancellationToken = default)
+	{
+		var JsonDocument = System.Text.Json.JsonSerializer.Serialize(objA);
+		var doc = BsonDocument.Parse(JsonDocument);
+		var id = doc["_id"];
+		return await collection.DeleteOneAsync($"{{ _id : '{id}' }}", cancellationToken);
+	}
+
+	public async static Task<UpdateResult> UpdateOneAsync(this IMongoCollection<BsonDocument> collection, object objA, CancellationToken cancellationToken = default)
+	{
+		var JsonDocument = System.Text.Json.JsonSerializer.Serialize(objA);
+		var doc = BsonDocument.Parse(JsonDocument);
+		var id = doc["_id"];
+		return await collection.UpdateOneAsync($"{{ _id : '{id}' }}", doc, new UpdateOptions(), cancellationToken);
+	}
+
+	public async static Task<IAsyncCursor<BsonDocument>> AggregateAsync(this IMongoCollection<BsonDocument> collection, string JsonDocuments, AggregateOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+		return await collection.AggregateAsync<BsonDocument>(bsonDocuments, options, cancellationToken);
+	}
+
+	public async static Task<long> CountAsync(this IMongoCollection<BsonDocument> collection, CountOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		return await collection.CountDocumentsAsync("{}", options, cancellationToken);
+	}
+
+	public async static Task<long> CountDocumentsAsync(this IMongoCollection<BsonDocument> collection, CountOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		return await collection.CountDocumentsAsync("{}", options, cancellationToken);
+	}
+
+	public async static Task<BulkWriteResult<BsonDocument>> BulkWriteAsync(this IMongoCollection<BsonDocument> collection, string JsonDocuments, WriteConcern? writeConcern = null, BulkWriteOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+
+		var requests = new List<WriteModel<BsonDocument>>();
+		foreach (var bsonDocument in bsonDocuments)
+		{
+			var first = bsonDocument.Elements.FirstOrDefault();
+			var doc = first.Value.AsBsonDocument;
+			switch (first.Name)
+			{
+				default:
+					break;
+				case "insertOne":
+					requests.Add(new InsertOneModel<BsonDocument>(doc.GetValue("document").AsBsonDocument));
+					break;
+				case "updateOne":
+					requests.Add(new UpdateOneModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument, doc.GetValue("update").AsBsonDocument));
+					break;
+				case "deleteOne":
+					requests.Add(new DeleteOneModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument));
+					break;
+				case "replaceOne":
+					requests.Add(new ReplaceOneModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument, doc.GetValue("replacement").AsBsonDocument));
+					break;
+				case "updateMany":
+					requests.Add(new UpdateManyModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument, doc.GetValue("update").AsBsonDocument));
+					break;
+				case "deleteMany":
+					requests.Add(new DeleteManyModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument));
+					break;
+			}
+		}
+
+		if (writeConcern != null)
+			return await collection.WithWriteConcern(writeConcern).BulkWriteAsync(requests, options, cancellationToken);
+		else
+			return await collection.BulkWriteAsync(requests, options, cancellationToken);
+	}
+
+	public async static Task<IChangeStreamCursor<BsonDocument>> WatchAsync(this IMongoCollection<BsonDocument> collection, string JsonDocuments, ChangeStreamOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+
+		return await collection.WatchAsync<BsonDocument>(bsonDocuments, options, cancellationToken);
+	}
+
+
+	// General
+	// ==============================================================================================================
+	public static IMongoCollection<BsonDocument> GetCollection(this IMongoDatabase db, string CollectionName, MongoCollectionSettings? settings = null)
+	{
+		return db.GetCollection<BsonDocument>(CollectionName, settings);
+	}
+
+	public static string Pretty(this List<BsonDocument> list)
+	{
+		return Serializer.ToJson(list, new JsonSerializerOptions() { WriteIndented = true });
+	}
+
+	public static string Pretty(this List<BsonValue> list)
+	{
+		return list.Select(x => x.ToBsonDocument()).ToList().Pretty();
+	}
+
+	public static string Pretty(this IAsyncCursor<BsonDocument> cursor)
+	{
+		return cursor.ToList().Pretty();
+	}
+
+	public static string Pretty(this IMongoCollection<BsonDocument> collection)
+	{
+		return collection.Find("{}").ToList().Pretty();
+	}
+
+
+	// Extensions synchronous
+	// ==============================================================================================================
+
+	public static BsonDocument RunCommand(this IMongoDatabase db, string JsonDocument, ReadPreference? readPreference = null)
+	{
+		return db.RunCommand<BsonDocument>(BsonDocument.Parse(JsonDocument), readPreference);
+	}
+
+	public static BsonValue InsertOne(this IMongoCollection<BsonDocument> collection, string JsonDocument, InsertOneOptions? options = null)
+	{
+		var doc = BsonDocument.Parse(JsonDocument);
+		collection.InsertOneAsync(doc, options);
+		return doc["_id"];
+	}
+
+	public static BsonValue InsertOne(this IMongoCollection<BsonDocument> collection, object objA, InsertOneOptions? options = null, CancellationToken cancellationToken = default)
+	{
+		return InsertOne(collection, System.Text.Json.JsonSerializer.Serialize(objA), options, cancellationToken);
+	}
+
+	public static List<BsonValue> InsertMany(this IMongoCollection<BsonDocument> collection, string JsonDocuments, InsertManyOptions? options = null)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+		collection.InsertManyAsync(bsonDocuments, options);
+		return bsonDocuments.Select(x => x["_id"]).ToList();
+	}
+
+	public static IAsyncCursor<BsonDocument> Aggregate(this IMongoCollection<BsonDocument> collection, string JsonDocuments, AggregateOptions? options = null)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+		return collection.Aggregate<BsonDocument>(bsonDocuments, options);
+	}
+
+	[Obsolete("Use CountDocuments or EstimatedDocumentCount instead.")]
+	public static long Count(this IMongoCollection<BsonDocument> collection, CountOptions? options = null)
+	{
+		return collection.Count("{}", options);
+	}
+
+	public static long CountDocuments(this IMongoCollection<BsonDocument> collection, CountOptions? options = null)
+	{
+		return collection.CountDocuments("{}", options);
+	}
+
+	public static BulkWriteResult<BsonDocument> BulkWrite(this IMongoCollection<BsonDocument> collection, string JsonDocuments, WriteConcern? writeConcern = null, BulkWriteOptions? options = null)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+
+		var requests = new List<WriteModel<BsonDocument>>();
+		foreach (var bsonDocument in bsonDocuments)
+		{
+			var first = bsonDocument.Elements.FirstOrDefault();
+			var doc = first.Value.AsBsonDocument;
+			switch (first.Name)
+			{
+				default:
+					break;
+				case "insertOne":
+					requests.Add(new InsertOneModel<BsonDocument>(doc.GetValue("document").AsBsonDocument));
+					break;
+				case "updateOne":
+					requests.Add(new UpdateOneModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument, doc.GetValue("update").AsBsonDocument));
+					break;
+				case "deleteOne":
+					requests.Add(new DeleteOneModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument));
+					break;
+				case "replaceOne":
+					requests.Add(new ReplaceOneModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument, doc.GetValue("replacement").AsBsonDocument));
+					break;
+				case "updateMany":
+					requests.Add(new UpdateManyModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument, doc.GetValue("update").AsBsonDocument));
+					break;
+				case "deleteMany":
+					requests.Add(new DeleteManyModel<BsonDocument>(doc.GetValue("filter").AsBsonDocument));
+					break;
+			}
+		}
+
+		if (writeConcern != null)
+			return collection.WithWriteConcern(writeConcern).BulkWrite(requests, options);
+		else
+			return collection.BulkWrite(requests, options);
+	}
+
+	public static IChangeStreamCursor<BsonDocument> Watch(this IMongoCollection<BsonDocument> collection, string JsonDocuments, ChangeStreamOptions? options = null)
+	{
+		var bsonDocuments = BsonSerializer.Deserialize<BsonDocument[]>(JsonDocuments);
+
+		return collection.Watch<BsonDocument>(bsonDocuments, options);
+	}
+
+
+}
