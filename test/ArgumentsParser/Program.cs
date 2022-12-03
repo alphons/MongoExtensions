70a3072f93ce6fbfdb8f18a878b675d70f1bc969
alphons <alphons@heijden.com>
2022-12-03 10:05:55 +0100

    for console

---
+++ test/ArgumentsParser/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using MongoTesting.ConsoleApp;
+
+//Helper.Parse(" f ( { a:'b', c:\"d\", e: [ aa: 'bb' , i : 123 ] }, {  a:'b', c:\"d\", e: [ aa: 'bb' , i : 123 ]} )");
+
+//Helper.Parse(" f (  [ { }, {}, {  }, {}    ]   )");
+
+//var af1 = Helper.ParseFunction(" f ( { }, {}, {  }, {}       )");
+
+//var af2 = Helper.ParseFunction("f(a )");
+
+var s = string.Empty;
+
+s = " ls -al";
+s = " f( a";
+s = " insert { _id: 'abc' }";
+
+var af = ParseHelper.ParseFunction(s);
+Console.WriteLine($"[{s}] Complete:{af.Complete} Function:{af.IsFunction} Name:{af.Name}");
+foreach (var a in af.Arguments)
+	Console.WriteLine($"\t{a}");
+
+var totalArguments = string.Join(' ', af.Arguments);
+Console.WriteLine("TotalArguments:" + totalArguments);
+Console.WriteLine("----");
+
+//var af4 = Helper.ParseFunction(" cls");
